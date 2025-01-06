e1bd58dcf759c46fcb68a3eeb751078c0a84ef95
Ragnarloki <sivasankarswaminathan62@gmail.com>
2025-01-06 22:22:33 +0530

    Reinstall dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -131,6 +131,9 @@ app.delete('/items/:id', async (req, res) => {
 });
 
 // Start Server
+app.get('/',(req,res)=>{
+  res.send("hello world");
+})
 app.listen(3000, () => {
   console.log('Server is running');
 });
