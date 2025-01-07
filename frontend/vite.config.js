175438c01240d9db662719391a4ab90ba0f02c58
Ragnarloki <sivasankarswaminathan62@gmail.com>
2025-01-07 12:13:58 +0530

    commited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/food_recipe_finder/",
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    mimeTypes: {
+      'text/css': ['css'],
+    },
+  },
   plugins: [react()],
 })
+
